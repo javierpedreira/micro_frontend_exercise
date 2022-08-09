1e65e3004b9ae47a3a3548ac262be411da1d70f4
Javier Pedreira <javier.pedreira@intenthq.com>
2022-08-09 09:47:18 +0200

    Adds an exposed component on fe1 that will be used from another component

--- fe1/next.config.js
+++ fe1/next.config.js
@@ -1,7 +1,37 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  webpack5: true, // Need to make it true for some versions of Next JS
+  distDir: 'build', // Defined build directory
+  webpack: (config, options) => { // webpack configurations
+    config.plugins.push(
+      new options.webpack.container.ModuleFederationPlugin({
+        name:"fe1",
+        filename: "remoteEntry.js", // remote file name which will used later
+        remoteType: "var",
+        exposes: { // expose all component here.
+          "./header": "./src/components/Header"
+        },
+        shared: [
+          {
+            react: {
+              eager: true,
+              singleton: true,
+              requiredVersion: false,
+            }
+          },
+          {
+            "react-dom": {
+              eager: true,
+              singleton: true,
+              requiredVersion: false,
+            }
+          },
+        ]
+      })
+    )
+    return config
+  }
 }
 
 module.exports = nextConfig
