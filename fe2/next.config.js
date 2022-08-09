6694fd28be10a7da29a445cfdaf128e8a83fd6f0
Javier Pedreira <javier.pedreira@intenthq.com>
2022-08-09 09:48:58 +0200

    Adds getSquareRoot to fe2 as an exposed method

--- fe2/next.config.js
+++ fe2/next.config.js
@@ -1,7 +1,36 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  distDir: 'build',
+  webpack: (config, options) => {
+    config.plugins.push(
+      new options.webpack.container.ModuleFederationPlugin({
+        name:"fe2",
+        filename: "remoteEntry_2.js",
+        remoteType: "var",
+        exposes: {
+          "./squareRoot": "./utils/getSquareRoot"
+        },
+        shared: [
+          {
+            react: {
+              eager: true,
+              singleton: true,
+              requiredVersion: false,
+            }
+          },
+          {
+            "react-dom": {
+              eager: true,
+              singleton: true,
+              requiredVersion: false,
+            }
+          },
+        ]
+      })
+    )
+    return config
+  }
 }
 
 module.exports = nextConfig
